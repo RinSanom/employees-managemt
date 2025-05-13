ad5d238bc712f0e6daaa527624241f34327d0e07
RinSanom <rinsanom3@gmail.com>
2025-05-13 13:49:55 -0700

    lasted

--- src/redux/features/apiSlice/apiSlice.js
+++ src/redux/features/apiSlice/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api",
+    baseUrl: "https://employees-magament-back-end-2.onrender.com/api",
 
     prepareHeaders: async (headers, { getState, endpoint }) => {
       const token = localStorage.getItem("accessToken");
